da3cd28388ca41966388be9eddd488dc9d782131
DonMins <maks000156@yandex.ru>
2020-09-21 21:53:11 +0400

    Первая страница готова

--- lib/main.dart
+++ lib/main.dart
@@ -12,19 +12,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
         primarySwatch: Colors.blue,
-        // This makes the visual density adapt to the platform that you run
-        // the app on. For desktop platforms, the controls will be smaller and
-        // closer together (more dense) than on mobile platforms.
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
       home: MyHomePage(title: 'Flutter Demo Home Page'),
@@ -35,15 +23,6 @@ class MyApp extends StatelessWidget {
 class MyHomePage extends StatefulWidget {
   MyHomePage({Key key, this.title}) : super(key: key);
 
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
   final String title;
 
   @override
@@ -51,27 +30,8 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
-
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
         backgroundColor: Colors.white,
         body: ListView(children: <Widget>[
@@ -114,178 +74,12 @@ class _MyHomePageState extends State<MyHomePage> {
                   style: TextStyle(color: Colors.grey),
                 )
               ])),
-          Container(
-              padding: EdgeInsets.only(top: 15.0, left: 22.0),
-              height: 70,
-              child: Row(children: <Widget>[
-                Container(
-                  width: 170.0,
-                  child: OutlineButton(
-                    padding: EdgeInsets.only(left: 10.0),
-                    child: Center(
-                        child: Container(
-                            child: Row(children: <Widget>[
-                      Container(
-                        height: 30.0,
-                        width: 30.0,
-                        child: new Icon(Icons.gesture, color: Colors.white),
-                        decoration: new BoxDecoration(
-                            color: Colors.blueAccent,
-                            borderRadius: new BorderRadius.circular(5.0)),
-                      ),
-                      Padding(padding: EdgeInsets.only(left: 10.0)),
-                      Text("Science",
-                          style: TextStyle(
-                            color: Colors.black,
-                          ))
-                    ]))),
-                    onPressed: null,
-                    shape: new RoundedRectangleBorder(
-                        borderRadius: new BorderRadius.circular(10.0)),
-                  ),
-                ),
-                Padding(padding: EdgeInsets.only(left: 20.0)),
-                Container(
-                    width: 170.0,
-                    height: 70.0,
-                    child: OutlineButton(
-                        padding: EdgeInsets.only(left: 5.0),
-                        child: Center(
-                            child: Container(
-                                child: Row(children: <Widget>[
-                          Container(
-                            height: 30.0,
-                            width: 30.0,
-                            child: new Icon(Icons.scanner, color: Colors.white),
-                            decoration: new BoxDecoration(
-                                color: Colors.pinkAccent,
-                                borderRadius: new BorderRadius.circular(5.0)),
-                          ),
-                          Padding(padding: EdgeInsets.only(left: 10.0)),
-                          Text(
-                            "Social",
-                            style: TextStyle(color: Colors.black),
-                          )
-                        ]))),
-                        onPressed: null,
-                        shape: new RoundedRectangleBorder(
-                            borderRadius: new BorderRadius.circular(10.0))))
-              ])),
-          Container(
-              padding: EdgeInsets.only(top: 15.0, left: 22.0),
-              height: 70,
-              child: Row(children: <Widget>[
-                Container(
-                  width: 170.0,
-                  child: OutlineButton(
-                    padding: EdgeInsets.only(left: 10.0),
-                    child: Center(
-                        child: Container(
-                            child: Row(children: <Widget>[
-                      Container(
-                        height: 30.0,
-                        width: 30.0,
-                        child: new Icon(Icons.terrain, color: Colors.white),
-                        decoration: new BoxDecoration(
-                            color: Colors.lightGreenAccent,
-                            borderRadius: new BorderRadius.circular(5.0)),
-                      ),
-                      Padding(padding: EdgeInsets.only(left: 10.0)),
-                      Text("Teach",
-                          style: TextStyle(
-                            color: Colors.black,
-                          ))
-                    ]))),
-                    onPressed: null,
-                    shape: new RoundedRectangleBorder(
-                        borderRadius: new BorderRadius.circular(10.0)),
-                  ),
-                ),
-                Padding(padding: EdgeInsets.only(left: 20.0)),
-                Container(
-                    width: 170.0,
-                    height: 70.0,
-                    child: OutlineButton(
-                        padding: EdgeInsets.only(left: 5.0),
-                        child: Center(
-                            child: Container(
-                                child: Row(children: <Widget>[
-                          Container(
-                            height: 30.0,
-                            width: 30.0,
-                            child: new Icon(Icons.gavel, color: Colors.white),
-                            decoration: new BoxDecoration(
-                                color: Colors.blueAccent,
-                                borderRadius: new BorderRadius.circular(5.0)),
-                          ),
-                          Padding(padding: EdgeInsets.only(left: 10.0)),
-                          Text(
-                            "Gaming",
-                            style: TextStyle(color: Colors.black),
-                          )
-                        ]))),
-                        onPressed: null,
-                        shape: new RoundedRectangleBorder(
-                            borderRadius: new BorderRadius.circular(10.0))))
-              ])),
-          Container(
-              padding: EdgeInsets.only(top: 15.0, left: 22.0),
-              height: 70,
-              child: Row(children: <Widget>[
-                Container(
-                  width: 170.0,
-                  child: OutlineButton(
-                    padding: EdgeInsets.only(left: 10.0),
-                    child: Center(
-                        child: Container(
-                            child: Row(children: <Widget>[
-                      Container(
-                        height: 30.0,
-                        width: 30.0,
-                        child: new Icon(Icons.history, color: Colors.white),
-                        decoration: new BoxDecoration(
-                            color: Colors.pinkAccent,
-                            borderRadius: new BorderRadius.circular(5.0)),
-                      ),
-                      Padding(padding: EdgeInsets.only(left: 10.0)),
-                      Text("History",
-                          style: TextStyle(
-                            color: Colors.black,
-                          ))
-                    ]))),
-                    onPressed: null,
-                    shape: new RoundedRectangleBorder(
-                        borderRadius: new BorderRadius.circular(10.0)),
-                  ),
-                ),
-                Padding(padding: EdgeInsets.only(left: 20.0)),
-                Container(
-                    width: 170.0,
-                    height: 70.0,
-                    child: OutlineButton(
-                        padding: EdgeInsets.only(left: 5.0),
-                        child: Center(
-                            child: Container(
-                                child: Row(children: <Widget>[
-                          Container(
-                            height: 30.0,
-                            width: 30.0,
-                            child: new Icon(Icons.account_balance,
-                                color: Colors.white),
-                            decoration: new BoxDecoration(
-                                color: Colors.lightGreenAccent,
-                                borderRadius: new BorderRadius.circular(5.0)),
-                          ),
-                          Padding(padding: EdgeInsets.only(left: 10.0)),
-                          Text(
-                            "Analytics",
-                            style: TextStyle(color: Colors.black),
-                          )
-                        ]))),
-                        onPressed: null,
-                        shape: new RoundedRectangleBorder(
-                            borderRadius: new BorderRadius.circular(10.0))))
-              ])),
+          _getRowButtons("Science", Colors.blueAccent, Icons.gesture, "Social",
+              Colors.pinkAccent, Icons.scanner),
+          _getRowButtons("Teach", Colors.lightGreenAccent, Icons.terrain,
+              "Gaming", Colors.blueAccent, Icons.gavel),
+          _getRowButtons("History", Colors.pinkAccent, Icons.history,
+              "Analytics", Colors.lightGreenAccent, Icons.account_balance),
           Padding(
               padding: EdgeInsets.only(left: 22.0, top: 30.0),
               child: Row(children: <Widget>[
@@ -299,16 +93,95 @@ class _MyHomePageState extends State<MyHomePage> {
             height: MediaQuery.of(context).size.height - 300,
             child: ListView(
               children: [
-                _getIconSurveys("icon/1.jpg", 'Food Survey'),
-                _getIconSurveys("icon/2.jpg", 'Business Survey'),
-                _getIconSurveys("icon/3.jpg", 'Travel Survey'),
+                _getIconSurveys("icon/1.jpg", 'Food Survey', 4 / 15, "4/15"),
+                _getIconSurveys(
+                    "icon/2.jpg", 'Business Survey', 1 / 10, "1/10"),
+                _getIconSurveys("icon/3.jpg", 'Travel Survey', 4 / 5, "4/5"),
               ],
             ),
           )
         ]));
   }
 
-  Widget _getIconSurveys(String imgPath, String name) {
+  Widget _getRowButtons(
+    String textName1,
+    Color color1,
+    IconData icon1,
+    String textName2,
+    Color color2,
+    IconData icon2,
+  ) {
+    return Container(
+        padding: EdgeInsets.only(top: 15.0, left: 22.0),
+        height: 70,
+        child: Row(children: <Widget>[
+          Container(
+            width: 170.0,
+            child: OutlineButton(
+              padding: EdgeInsets.only(left: 10.0),
+              child: Center(
+                  child: Container(
+                      child: Row(children: <Widget>[
+                Container(
+                  height: 30.0,
+                  width: 30.0,
+                  child: new Icon(icon1, color: Colors.white),
+                  decoration: new BoxDecoration(
+                      color: color1,
+                      borderRadius: new BorderRadius.circular(5.0)),
+                ),
+                Padding(padding: EdgeInsets.only(left: 10.0)),
+                Text(textName1,
+                    style: TextStyle(
+                      color: Colors.black,
+                      fontSize: 18.0,
+                      fontWeight: FontWeight.bold,
+                    ))
+              ]))),
+              onPressed: () {
+                // Navigate to second route when tapped.
+              },
+              shape: new RoundedRectangleBorder(
+                  borderRadius: new BorderRadius.circular(10.0)),
+            ),
+          ),
+          Padding(padding: EdgeInsets.only(left: 20.0)),
+          Container(
+              width: 170.0,
+              height: 70.0,
+              child: OutlineButton(
+                  padding: EdgeInsets.only(left: 10.0),
+                  child: Center(
+                      child: Container(
+                          child: Row(children: <Widget>[
+                    Container(
+                      height: 30.0,
+                      width: 30.0,
+                      child: new Icon(icon2, color: Colors.white),
+                      decoration: new BoxDecoration(
+                          color: color2,
+                          borderRadius: new BorderRadius.circular(5.0)),
+                    ),
+                    Padding(padding: EdgeInsets.only(left: 10.0)),
+                    Text(
+                      textName2,
+                      style: TextStyle(
+                        color: Colors.black,
+                        fontSize: 18.0,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    )
+                  ]))),
+                  onPressed: () {
+                    // Navigate to second route when tapped.
+                  },
+                  shape: new RoundedRectangleBorder(
+                      borderRadius: new BorderRadius.circular(10.0))))
+        ]));
+  }
+
+  Widget _getIconSurveys(
+      String imgPath, String name, double percent, description) {
     return Padding(
         padding: EdgeInsets.only(left: 22, right: 10, top: 10),
         child: InkWell(
@@ -317,7 +190,6 @@ class _MyHomePageState extends State<MyHomePage> {
               mainAxisAlignment: MainAxisAlignment.spaceBetween,
               children: <Widget>[
                 Container(
-
                   child: Row(
                     children: [
                       Hero(
@@ -330,19 +202,52 @@ class _MyHomePageState extends State<MyHomePage> {
                         ),
                       ),
                       SizedBox(width: 10),
-
                       Column(
                         crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          Text(
-                            name,
-                            style: TextStyle(
-                              fontFamily: 'Montserrat',
-                              fontWeight: FontWeight.bold,
-                            ),
+                        children: <Widget>[
+                          Container(
+                            width: 300,
+                            child: Row(
+                                mainAxisAlignment:
+                                    MainAxisAlignment.spaceBetween,
+                                children: [
+                                  Text(
+                                    name,
+                                    textAlign: TextAlign.left,
+                                    style: TextStyle(
+                                      fontFamily: 'Montserrat',
+                                      fontWeight: FontWeight.bold,
+                                    ),
+                                  ),
+                                  Text(
+                                    description,
+                                    textAlign: TextAlign.right,
+                                    style: TextStyle(
+                                      fontFamily: 'Montserrat',
+                                      fontWeight: FontWeight.bold,
+                                    ),
+                                  ),
+                                ]),
                           ),
+                          Center(
+                              child: Padding(
+                            padding: EdgeInsets.only(top: 10),
+                            child: Container(
+                              width: 300,
+                              child: ClipRRect(
+                                borderRadius:
+                                    BorderRadius.all(Radius.circular(10)),
+                                child: LinearProgressIndicator(
+                                  backgroundColor: Colors.grey,
+                                  valueColor: new AlwaysStoppedAnimation<Color>(
+                                      Colors.blueAccent),
+                                  value: percent,
+                                ),
+                              ),
+                            ),
+                          )),
                         ],
-                      )
+                      ),
                     ],
                   ),
                 )
